f57465c8f10f7b70237fea1b5a1fffb58ec8b06b
Danny Caulwell <caulwell@protonmail.com>
2022-06-02 21:04:14 +0100

    allow add job to db

--- server/Controllers/JobController.cs
+++ server/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using unter.Models;
+using unter.Data;
 
 namespace unter.Controllers;
 
@@ -8,11 +9,27 @@ namespace unter.Controllers;
 public class JobController : ControllerBase
 {
 
+    private readonly ApplicationDbContext _db;
+
+    public JobController(ApplicationDbContext db){
+        _db = db;
+    }
+
+    [HttpGet]
+    public IActionResult Get(){
+
+        IEnumerable<Job> jobsList = _db.Jobs;
+        return Ok(jobsList);
+        
+    }
+
+
 
     [HttpPost]
     public IActionResult Post(Job job)
     {
-
+        _db.Jobs.Add(job);
+        _db.SaveChanges();
         return CreatedAtAction(nameof(Post), new {id = job.Id}, job);
     }
 
