8e97c234a1e51e3a24c0da20b824765a71a8beb4
Danny Caulwell <caulwell@protonmail.com>
2022-07-02 17:52:31 +0100

    allow logout

--- client/src/components/ControlPanel.tsx
+++ client/src/components/ControlPanel.tsx
@@ -1,6 +1,7 @@
 import {AiFillFilter} from "react-icons/ai";
 import {MdCreate} from "react-icons/md";
 import {FaMapMarkerAlt} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 interface ControlPanelProps{
     toggleCreateModal: () => void;
@@ -9,10 +10,18 @@ interface ControlPanelProps{
 
 const ControlPanel = ({toggleCreateModal} : ControlPanelProps) => {
 
+    const navigate = useNavigate();
+
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
 
 
     return (
         <div className="flex  justify-between w-full px-8 space-x-8 items-center h-12">
+            <div className="flex space-x-6">
             <button className="p-1 px-2 text-white font-bold text-sm bg-violet-800 rounded hover:shadow-md flex items-center space-x-2">
                 <AiFillFilter/> <span>Filter</span>
             </button>
@@ -24,6 +33,10 @@ const ControlPanel = ({toggleCreateModal} : ControlPanelProps) => {
             </div>
             <button onClick={toggleCreateModal}  className="p-1 px-2 text-white font-bold text-sm bg-violet-800 rounded hover:shadow-md flex items-center space-x-2">
             <MdCreate/> <span>Add Job</span>
+            </button>
+            </div>
+            <button onClick={handleLogout}  className="p-1 px-2 text-white font-bold text-sm bg-violet-800 rounded hover:shadow-md flex items-center space-x-2">
+            <MdCreate/> <span>Logout</span>
             </button>
             
         </div>
